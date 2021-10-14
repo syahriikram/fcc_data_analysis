a49261ee858eaa2cb29068c153a0c552a357594e
syahriikram <syahriikram@gmail.com>
2021-10-14 19:40:00 +0800

    Completed MVSD Calculator

---
+++ 1_M_V_SD_Calculator/mean_var_std.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def calculate(list):
+
+  if len(list) < 9:
+    raise ValueError("List must contain nine numbers.")
+  else:
+    matrix = np.array(list)
+    matrix = matrix.reshape(3,3)
+
+    calculations = {}
+
+    calculations["mean"] = [np.mean(matrix, axis=0).tolist(), np.mean(matrix, axis=1).tolist(), np.mean(matrix).tolist()]
+    calculations["variance"] = [np.var(matrix, axis=0).tolist(), np.var(matrix, axis=1).tolist(), np.var(matrix).tolist()]
+    calculations["standard deviation"] = [np.std(matrix, axis=0).tolist(), np.std(matrix, axis=1).tolist(), np.std(matrix).tolist()]
+    calculations["max"] = [np.max(matrix, axis=0).tolist(), np.max(matrix, axis=1).tolist(), np.max(matrix).tolist()]
+    calculations["min"] = [np.min(matrix, axis=0).tolist(), np.min(matrix, axis=1).tolist(), np.min(matrix).tolist()]
+    calculations["sum"] = [np.sum(matrix, axis=0).tolist(), np.sum(matrix, axis=1).tolist(), np.sum(matrix).tolist()]
+
+    return calculations
---
+++ README.md
@@ -0,0 +1,11 @@
+# FreeCodeCamp - Data Analysis with Python
+
+## Summary / details:
+- A free course by freecodecamp.org which contains video lectures about Data Analysis, Pandas and Numpy with 5 different mini projects.
+- Upon completion of 5 mini projects, a certification is awarded
+
+## 1st project: Mean Variance Standard Deviation Calculator
+- Development was done on replit: https://replit.com/@SyahriIkram/boilerplate-mean-variance-standard-deviation-calculator
+- Related codes can be found in the 1_M_V_SD_Calculator folder
+- Objective: using numpy standard functions and conversion between lists
+
